2f856be620b5d50aba84fa1c889e7c4f1ddbb3e8
shreyamishra414@gmail.com <shreyamishra414@gmail.com>
2022-07-31 16:01:49 +0530

    Ques 1877 and 605

---
+++ greedy/Can_place_flowers.cpp
@@ -0,0 +1,147 @@
+/*
+605. Can Place Flowers
+Easy
+
+You have a long flowerbed in which some of the plots are planted, and some are not. However, flowers cannot be planted in adjacent plots.
+
+Given an integer array flowerbed containing 0's and 1's, where 0 means empty and 1 means not empty, and an integer n, return if n new flowers
+can be planted in the flowerbed without violating the no-adjacent-flowers rule.
+
+
+
+Example 1:
+
+Input: flowerbed = [1,0,0,0,1], n = 1
+Output: true
+Example 2:
+
+Input: flowerbed = [1,0,0,0,1], n = 2
+Output: false
+
+
+Constraints:
+
+1 <= flowerbed.length <= 2 * 104
+flowerbed[i] is 0 or 1.
+There are no two adjacent flowers in flowerbed.
+0 <= n <= flowerbed.length
+
+*/
+
+// Runtime: 39 ms, faster than 16.68% of C++ online submissions for Can Place Flowers.
+// Memory Usage: 20.2 MB, less than 88.18% of C++ online submissions for Can Place Flowers.
+
+
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution
+{
+public:
+    bool canPlaceFlowers(vector<int> &flowerbed, int n)
+    {
+        /*
+        Algorithm
+
+        n is the number of new flowers
+        and no two flowers cannot be planted in adjacent plots
+
+        1. Since the flowerbed array is in 0 and 1, check the boundary conditions seprately
+        2. If it has to be present in the starting of the vector, - check if flowerbed[0] ==0 and flowerbed [1] == 0 as well
+        3. If it has to be planted in the middle, check the current to be zero and similarly , for the previous and the next.
+        4. If it has to be planted in the end of the vector, check if the last element is zero (flowerbed [flowerbed.size()-1]==0) 
+        and second last element is also zero
+        5. Go in the same order as above
+        6. Keep a check of n always so that it doesn't go below 0
+        */
+
+        // checking for the first position
+
+        // cout << "initially \n"; 
+        // for (int i=0;i<flowerbed.size();i++){
+        //     for (int j=0;j<60; j++)
+        //         cout << flowerbed[i++]<< " ";
+        //     cout<< endl;
+            
+        // }
+
+        if (n==0){
+            return true;
+        }
+        if (flowerbed.size()==0 || (flowerbed.size()==1 && flowerbed[0]==1 && n>0) ){
+            return false;
+        }
+        if ((flowerbed.size()==1 && flowerbed[0]==0)){
+            flowerbed[0]=1;
+            n--;
+            cout << n <<endl;
+
+        }
+        
+        // flowerbed array of size 2 can be taken care with this test case itself
+        if (flowerbed[0]==0 && flowerbed[1]==0 && n>0){
+            n--;
+            cout << n <<endl;
+            flowerbed[0]=1;
+        }
+
+        if (flowerbed.size() >= 3 && n!=0){
+            // checking for middle element
+            int i;
+            for (i=1;i<flowerbed.size()-1; i++){
+                if(flowerbed[i]==0 && flowerbed[i-1]==0 && flowerbed[i+1]==0 && n>0){
+
+                    n--;
+                    cout << n <<endl;
+
+                    flowerbed[i]= 1;
+                }
+            }
+            // i already stores the last index 
+            if (flowerbed[i]==0 && flowerbed[i-1]==0 && n>0){
+                n--;
+                cout << n <<endl;
+
+                flowerbed[i]=1;
+            }
+        }
+
+        cout << "n="<< n <<endl;
+        // cout << "\nafter planting \n"; 
+
+        // for (int i=0;i<flowerbed.size();i++){
+        //     for (int j=0;j<60; j++)
+        //         cout << flowerbed[i++]<< " ";
+        //     cout<< endl;
+        // }
+
+
+        return n == 0; // n will be zero if all the flowers get ploted
+    }
+};
+
+
+int main(int argc, char const *argv[])
+{
+    
+    vector <int> fb={0,0,0,0,0};
+    int n=2;
+
+    fb = {0,1,0,1,0,1,0,0,1,0,0,0,0,1,0,0,1,0,1,0,1,0,1,0,1,0,0,0,1,0,1,0,0,0,0,0,0,1,0,0,1,
+    0,1,0,1,0,0,0,1,0,1,0,1,0,1,0,1,0,0,1,0,0,0,
+    0,0,1,0,1,0,1,0,1,0,0,1,0,1,0,0,1,0,1,0,1,0,1,0,
+    1,0,1,0,0,0,1,0,1,0,0,0,0,1,0,1,0,1,0,1,0,0,1,0,0,0,1,0,1,0,0,0,0,0,1,0,0,0,0,0,0,0,1,0,1,0,
+    1,0,0,0,1,0,0,0,0,0,0,0,0,1,0,0,1,0,1,0,0,1,0,1,0,0,1,0,1,0,1,0,1,0,1,0,1,0,0,0,1,0,1,0,0,0,0,1,0,
+    0,0,1,0,0,1,0,0,0,1,0,1,0,0,1,0,0,0,0,0,0,0,0,0,0,1,0,1,0,1,0,0,0,1,0,0,1,0,1,0,0,1,0,0,1,0,1,0,1,0,
+    1,0,1,0,1,0,0,1,0,1,0,0,1,0,0,0,0,1,0,1,0,1,0,0,1,0,1,0,0,1,0,0,1,0,0,1,0,1,0,1,0,0,0,1,0,1,0,0,1,0,1,0,
+    1,0,1,0,1,0,1,0,0,0,1,0,0,1,0,0,1,0,1,0,0,1,0,0,0,1,0,0,1,0,1,0,1,0,0,0,1,0,0,0,1,0,1,0,1,0,0,0,1,0,0,1,0,
+    0,1,0,0,0,0,1,0,0,0,0,0,1,0,1,0,1,0,1,0,0,1,0,1,0,1,0,0,0,1,0,0,0,1,0,0,1,0,1,0,0,0,1,0,0,0,1,0,0,1,0,1,0,
+    1,0,0,1,0,1,0,1,0,1,0,1,0,1,0,0,1,0,1,0,1,0,1,0,0,1,0,1,0,0,0,1,0,1,0,1,0,1,0,0,1,0,0,1,0,0,0,1,0,1,0,1,0,
+    0,0,0,0,1,0,0,0,1,0,1,0,0,0,0,1,0,1,0,1,0,0,0,1,0,1,0,1,0,0,1,0,0,1,0,1,0,1,0,1,0,0,1,0,0,1,0,1,0,0,0,1,0,
+    0,0,1,0,0,1,0,1,0,0,0,1,0,1,0,0,1,0,0,0};
+    n=25;
+    Solution a;
+    cout<<a.canPlaceFlowers(fb,n);
+    return 0;
+}
+
---
+++ greedy/Minimize_maximum_pair_sum_in _a_array.cpp
@@ -0,0 +1,92 @@
+/*
+1877. Minimize Maximum Pair Sum in Array
+Medium
+
+The pair sum of a pair (a,b) is equal to a + b. The maximum pair sum is the largest pair sum in a list of pairs.
+
+For example, if we have pairs (1,5), (2,3), and (4,4), the maximum pair sum would be max(1+5, 2+3, 4+4) = max(6, 5, 8) = 8.
+Given an array nums of even length n, pair up the elements of nums into n / 2 pairs such that:
+
+Each element of nums is in exactly one pair, and
+The maximum pair sum is minimized.
+Return the minimized maximum pair sum after optimally pairing up the elements.
+
+
+
+Example 1:
+
+Input: nums = [3,5,2,3]
+Output: 7
+Explanation: The elements can be paired up into pairs (3,3) and (5,2).
+The maximum pair sum is max(3+3, 5+2) = max(6, 7) = 7.
+Example 2:
+
+Input: nums = [3,5,4,2,4,6]
+Output: 8
+Explanation: The elements can be paired up into pairs (3,5), (4,4), and (6,2).
+The maximum pair sum is max(3+5, 4+4, 6+2) = max(8, 8, 8) = 8.
+
+
+Constraints:
+
+n == nums.length
+2 <= n <= 105
+n is even.
+1 <= nums[i] <= 105
+
+*/
+
+// Runtime: 734 ms
+// Memory Usage: 102 MB, 
+
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution
+{
+public:
+    int minPairSum(vector<int> &nums)
+    {
+        /*
+        Algorithm
+
+        1. Sort in decending order
+        2. Now add the first and last elements to create a pair
+        3. repeat the same all the way till the middle
+        ( 8,6,5,2 - in this case pairwise sum max can be in the middle)
+        4. create another vector to store the pairwise sum of size len/2
+        5. find the maximum number
+        */
+
+        // Sort the vector in descending order
+        sort(nums.begin(), nums.end(), greater<int>());
+        vector<int> pair;
+        int n = nums.size();
+        for (int i = 0; i < n / 2; i++)
+        {
+            pair.push_back( nums[i] + nums[n - i - 1]);
+            cout<< pair[i];
+        }
+
+        // find maximum value in the vector
+        int m = pair[0];
+        for (int i = 1; i < n / 2; i++)
+        {
+            if (pair[i] >m)
+            {
+                m = pair[i];
+            }
+        }
+
+        return m;
+    }
+};
+
+
+int main(int argc, char const *argv[])
+{
+    Solution a;
+    vector <int> num = {3,5,2,3};
+    cout<<a.minPairSum(num);
+    return 0;
+}
